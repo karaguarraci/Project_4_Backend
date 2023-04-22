2ab2b34da80fbb98f7dec6a54e0d2c01115e74a5
Kara Guarraci <karaguarraci@gmail.com>
2023-04-22 14:21:06 +0100

    add image option to review model

--- reviews/models.py
+++ reviews/models.py
@@ -5,6 +5,7 @@ class Review(models.Model):
     restaurant = models.ForeignKey('restaurants.Restaurant', on_delete=models.CASCADE, related_name='reviews')
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     comment = models.TextField(blank=True)
+    images = models.ImageField(upload_to='images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(
         'jwt_auth.User', 
